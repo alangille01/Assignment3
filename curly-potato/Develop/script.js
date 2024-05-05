ab78f7965f1e7655fdf43e06eabce9b27cceb4b8
Alexis Langille <lex@Alexiss-Laptop.local>
2024-05-05 12:18:19 -0400

    test commit

--- curly-potato/Develop/script.js
+++ curly-potato/Develop/script.js
@@ -5,7 +5,7 @@ const addEmployeesBtn = document.querySelector('#add-employees-btn');
 const collectEmployees = function() {
   // TODO: Get user input to create and return an array of employee objects
 }
-
+// test comment 
 // Display the average salary
 const displayAverageSalary = function(employeesArray) {
   // TODO: Calculate and display the average salary
